79576f1859098263f705139d178a489cfd9f2f31
Pawan Shrivastava <pawanshrivastav55@gmail.com>
2017-03-12 22:32:54 -0700

    First Commit

--- TipperCalc/ViewController.swift
+++ TipperCalc/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totallabel: UILabel!
+    @IBOutlet weak var tiplabel: UILabel!
+     @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+   
+    @IBAction func calculatortip(sender: AnyObject) {
+        
+        let tipPercentages = [ 0.18, 0.2 , 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tiplabel.text = String(format :"%f.2f",tip)
+        totallabel.text = String(format :"%f.2f",total)
+    }
+    
+    @IBAction func calculator(_ sender: Any) {
+    }
 
+    @IBAction func onTap(sender: AnyObject ) {
+        
+        view.endEditing(true)
+    }
 }
 
